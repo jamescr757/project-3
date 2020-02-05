7a84660ff004cae2d632e3bd5b89a706f4703ff4
jamescr757 <55260759+jamescr757@users.noreply.github.com>
2020-02-05 08:56:54 -0600

    refactored send email now code to use async and await. tested and works

--- controllers/emailScoresNow.js
+++ controllers/emailScoresNow.js
@@ -2,22 +2,15 @@ const db = require("../models");
 const Op = db.Sequelize.Op;
 const moment = require("moment");
 
-module.exports = function emailScoresNow(req, res) {
+function loopThruEmailData(data) {
 
     const userGameInfo = [];
-    
-    db.EmailData
-        .findAll({
-            where: {
-                email: req.params.email
-            }
-        })
-        .then((data) => {
 
-            data.forEach((entry, index) => {
+    return new Promise(resolve => {
+        data.forEach(async (entry, index) => {
 
             const { category, identifier, frequency, completedTable, futureTable } = entry.dataValues;
-
+    
             const whereObj = {};
             switch (category) {
                 case "division":
@@ -30,71 +23,57 @@ module.exports = function emailScoresNow(req, res) {
                 identifier === "All Teams" ? "" : whereObj[Op.or] = [{homeTeamConference: identifier}, {awayTeamConference: identifier}]
                 break;
             }
-
+    
             if (completedTable) {
                 const endDate = moment().utcOffset(-7).subtract(1, "days").format("YYYYMMDD");
                 const startDate = moment().utcOffset(-7).subtract(frequency, "days").format("YYYYMMDD");
-
+    
                 whereObj.date = { [Op.between]: [startDate, endDate] };
-
-                db.Completed
-                    .findAll({
+    
+                const scores = await db.Completed.findAll({
                         where: whereObj,
                         order: [[ "date", "DESC" ]]
                     })
-                    .then(scores => {
-                        scores.forEach(game => {
-                        userGameInfo.push(game.dataValues);
-                        })
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
+    
+                scores.forEach(game => userGameInfo.push(game.dataValues));
             }
-
+    
             if (futureTable) {
                 const startDate = moment().utcOffset(-7).format("YYYYMMDD");
                 const endDate = moment().utcOffset(-7).add(parseInt(frequency) - 1, "days").format("YYYYMMDD");
-
+    
                 whereObj.date = { [Op.between]: [startDate, endDate] };
-
-                db.Future
-                    .findAll({
+    
+                const games = await db.Future.findAll({
                         where: whereObj,
                         order: [[ "date", "ASC" ]]
                     })
-                    .then(games => {
-                        games.forEach(game => {
-                        userGameInfo.push(game.dataValues);
-                        })
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
+                
+                games.forEach(game => userGameInfo.push(game.dataValues));
             }
-
-            })
+    
+            if (index === data.length - 1) {
+                resolve(userGameInfo);
+            }
         })
-        .then(()=>{
+    })
+}
 
-            setTimeout(() => {
-                if (userGameInfo.length) {
-                    res.send("success");
-                } else {
-                    res.send("no games")
-                }
-            }, 3 * 1000);
-
-            setTimeout(() => {
-                if (userGameInfo.length) {
-                    require("../email/email")(userGameInfo, req.params.email)
-                }
-            }, 10 * 1000);
-
+module.exports = async function emailScoresNow(req, res) {
+    
+    const data = await db.EmailData.findAll({
+            where: {
+                email: req.params.email
+            }
         })
-        .catch(err => {
-            console.log(err);
-            console.log("error with EmailData query while emailing the scores");
-        });
+    
+    const userGameInfo = await loopThruEmailData(data);
+
+    if (userGameInfo.length) {
+        res.send("success");
+        require("../email/email")(userGameInfo, req.params.email)
+    } else {
+        res.send("no games")
+    }
 
 }
